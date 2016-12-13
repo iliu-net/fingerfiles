3dc9799c685c26ac4174e6b2920df3ad13542b1f
Alejandro Liu <alejandro_liu@hotmail.com>
2016-12-13 23:12:32 +0100

    Fixing bugs

--- src/views/default.php
+++ src/views/default.php
@@ -1,43 +1,43 @@
 <html>
-	<head>
-		<title>FingerFile</title>
-	</head>
-	<body>
-		<h1>FingerFile</h1>
+  <head>
+    <title>FingerFile</title>
+  </head>
+  <body>
+    <h1>FingerFile</h1>
 <?php
 require_once(LIBDIR.'catalogue.php');
 require_once(LIBDIR.'units.php');
 
 $cat = read_catalogue();
 if (count($cat)) {
-	echo '<table>';
-	echo '<tr><th align="left">Object</th><th>Size</th><th>Date/Time</th></tr>';
-	foreach ($cat as $i=>$j) {
-		echo '<tr>';
-		if (file_exists(INCOMING_DIR.$i)) {
-		echo '<td>';
-			echo '<a href="'.$_SERVER['SCRIPT_NAME'].'/repo/'.urlencode($i).'">';
-			echo htmlspecialchars($i).'</a>';
-			echo '</td>';
-			echo '<td>';
-			echo FileSizeConvert(filesize(INCOMING_DIR.$i));
-			echo '</td>';
-			echo '<td>';
-			echo date('Y-m-d H:i',filemtime(INCOMING_DIR.$i));
-			echo '</td>';
-		} else {
-			echo '<td>'.htmlspecialchars($i).'</td>';
-			echo '<td colspan="2">Missing!</td>';
-		}
-		echo '</td>';
-		echo '</tr>';
-	}
-	echo '</table>';
+  echo '<table>';
+  echo '<tr><th align="left">Object</th><th>Size</th><th>Date/Time</th></tr>';
+  foreach ($cat as $i=>$j) {
+    echo '<tr>';
+    if (file_exists(INCOMING_DIR.$i)) {
+      echo '<td>';
+      echo '<a href="'.$_SERVER['SCRIPT_NAME'].'/repo/'.urlencode($i).'">';
+      echo htmlspecialchars($i).'</a>';
+      echo '</td>';
+      echo '<td>';
+      echo FileSizeConvert(filesize(INCOMING_DIR.$i));
+      echo '</td>';
+      echo '<td>';
+      echo date('Y-m-d H:i',filemtime(INCOMING_DIR.$i));
+      echo '</td>';
+    } else {
+      echo '<td>'.htmlspecialchars($i).'</td>';
+      echo '<td colspan="2">Missing!</td>';
+    }
+    echo '</td>';
+    echo '</tr>';
+  }
+  echo '</table>';
 }
 
 ?>
-		<p>
-			<a href="<?= $_SERVER['SCRIPT_NAME'].'/manage' ?>">Manage</a>
-		</p>
-	</body>
+    <p>
+	    <a href="<?= $_SERVER['SCRIPT_NAME'].'/manage' ?>">Manage</a>
+    </p>
+  </body>
 </html>
